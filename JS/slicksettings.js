f0984ebc0778d8e43369a7a60b3e8a73cbcc707d
Wojciech Fic <ficwojciech95@gmail.com>
2023-04-18 18:34:24 +0200

    added new section, added new scss

--- JS/slicksettings.js
+++ JS/slicksettings.js
@@ -5,4 +5,24 @@ $('.team-carousel').slick({
 	infinite: true,
 	slidesToShow: 1,
 	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+	],
 })
